4d664c4f16844a687eab22023dee4bbf79809743
yogi-karnati <80158769+yogi-karnati@users.noreply.github.com>
2022-11-27 23:27:45 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,25 @@
+class BinarySearch{  
+ public static void binarySearch(int arr[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }  
+ }  
+ public static void main(String args[]){  
+        int arr[] = {100,200,300,400,1000,2000,3000,4000};  
+        int key = 30;  
+        int last=arr.length-1;  
+        binarySearch(arr,0,last,key);     
+ }  
+}  
